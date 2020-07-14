fcfcff2338d5099d74d6526dadb6de79607c9069
sotteh7 <sotetteh7@gmail.com>
2020-07-14 11:39:31 -0400

    Shopping Cart Updates

---
+++ js/prompt.js
@@ -0,0 +1,9 @@
+var myName = prompt("What is your first & last name?");
+
+var ageNumber = prompt("What is your age?");
+
+var locationCity = prompt("What City are you located in?");
+
+var locationState = prompt("State");
+
+console.log(myName + ' is ' + ageNumber + ' yrs old, and lives in ' + locationCity + ', ' + locationState);
